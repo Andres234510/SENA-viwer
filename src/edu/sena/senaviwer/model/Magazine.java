07e3362e31d13e2fa688ed4dd4c97e0eb0fc7773
andre <andres2345@gmail.com>
2024-08-17 09:47:54 -0500

    Se realizo la clase Magazine.java

--- src/edu/sena/senaviwer/model/Magazine.java
+++ src/edu/sena/senaviwer/model/Magazine.java
@@ -1,4 +1,18 @@
 package edu.sena.senaviwer.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Magazine {
+    int id;
+    String title;
+    Date editorialDate;
+    String editorial;
+    List<String> authors;
+
+    public Magazine(String title, Date editorialDate, String editorial){
+        this.title = title;
+        this.editorialDate = editorialDate;
+        this.editorial = editorial;
+    }
 }
