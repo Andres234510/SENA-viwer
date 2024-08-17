64912573e8057547ccbb1f9c59b1563545e4ed56
andre <andres2345@gmail.com>
2024-08-17 09:09:11 -0500

    Se realizo la clase Chapter

--- src/edu/sena/senaviwer/model/Chapter.java
+++ src/edu/sena/senaviwer/model/Chapter.java
@@ -2,5 +2,16 @@ package edu.sena.senaviwer.model;
 
 public class Chapter {
     int id;
+    String title;
+    int duration;
+    int year;
+    boolean viewed;
+    int timeViwed;
+    int sessionNumber;
 
+    public Chapter(String title, int duration, int year){
+        this.title = title;
+        this.duration = duration;
+        this.year = year;
+    }
 }
