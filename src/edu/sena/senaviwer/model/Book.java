b55f93551d015ad03e946d95dd40b38854cb43d0
andre <andres2345@gmail.com>
2024-08-17 09:27:59 -0500

    Se realizo la clase BooK

--- src/edu/sena/senaviwer/model/Book.java
+++ src/edu/sena/senaviwer/model/Book.java
@@ -1,4 +1,22 @@
 package edu.sena.senaviwer.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Book {
+    int id;
+    String title;
+    Date editorialDate;
+    String editorial;
+    List<String> authors;
+    String isbn;
+    boolean readed;
+    int timeReaded;
+
+    public Book(String title, Date editorialDate, String editorial, String isbn){
+        this.title = title;
+        this.editorialDate = editorialDate;
+        this.editorial = editorial;
+        this.isbn = isbn;
+    }
 }
