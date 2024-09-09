768b856ed0c6eda76e0dd4e71240af47d1bdc63e
andre <andres2345@gmail.com>
2024-09-09 07:17:19 -0500

    refacorizamos mavi y serie y creamos la clase padre Film.java

--- src/edu/sena/senaviwer/model/Serie.java
+++ src/edu/sena/senaviwer/model/Serie.java
@@ -1,64 +1,14 @@
 package edu.sena.senaviwer.model;
 
-public class Serie {
-    int id;
-    String title;
-    String genre;
-    String creator;
-    int duration;
-    int year;
-    boolean viewed;
+public class Serie extends Film{
     int timeViewed;
     int sessionQuantity;
 
     public Serie(String title, String genre, int duration) {
-        this.title = title;
-        this.genre = genre;
-        this.duration = duration;
+        super(title, genre, duration);
         this.viewed = true;
     }
 
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public String getGenre(){
-        return genre;
-    }
-
-    public void setGenre(String genre){
-        this.genre = genre;
-    }
-
-    public int getDuration(){
-        return duration;
-    }
-
-    public void setDuration(int duration){
-        this.duration = duration;
-    }
-
-    public boolean getViewed(){
-        return viewed;
-    }
-
-    public void setViewed(boolean viewed){
-        this.viewed =viewed;
-    }
 
     public int getTimeViewed(){
         return timeViewed;
